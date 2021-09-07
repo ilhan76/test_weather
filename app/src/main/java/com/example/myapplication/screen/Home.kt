5727e6354e9af88be8a0112b6bfa3cc39394aeab
ilhan76 <kudashov.ilya@inbox.ru>
2021-09-07 13:00:11 +0300

    Added home fragment.

---
+++ app/src/main/java/com/example/myapplication/screen/Home.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication.screen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentHomeBinding
+
+class Home : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        init()
+        return binding.root
+    }
+
+    private fun init(){
+
+    }
+}
